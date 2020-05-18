9bd1ecd06a2198552d2fe04a95a20cc37e945b56
uhunn <wxvv3386@gmail.com>
2020-05-18 23:10:34 +0900

    시작 설정 버튼 활성화

--- app/src/main/java/com/example/rspp/MainActivity.java
+++ app/src/main/java/com/example/rspp/MainActivity.java
@@ -2,10 +2,16 @@ package com.example.rspp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    TextView textView_start;
+    TextView textView_setting;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,6 +19,25 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
-        System.out.println("www");
+        textView_start = findViewById(R.id.tv_Start);
+        textView_setting = findViewById(R.id.tv_setting);
+
+
+        textView_start.setClickable(true);
+        textView_start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, StartActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        textView_setting.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, SettingActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
