958272c81fc848a2e83466a2e21751e1973818ba
uhunn <wxvv3386@gmail.com>
2020-05-19 18:08:12 +0900

    자바 버튼 함수 아직 안넣음

--- app/src/main/java/com/example/rspp/StartActivity.java
+++ app/src/main/java/com/example/rspp/StartActivity.java
@@ -1,10 +1,18 @@
 package com.example.rspp;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class StartActivity extends AppCompatActivity {
+
+    Button bt_r;
+    Button bt_s;
+    Button bt_p;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
