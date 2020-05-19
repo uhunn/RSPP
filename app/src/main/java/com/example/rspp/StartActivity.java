21d561a2144d237b267589c02bcdf9d1cf1dbbbd
uhunn <wxvv3386@gmail.com>
2020-05-19 22:43:03 +0900

    애니메이션 부분 다시 확인하기

--- app/src/main/java/com/example/rspp/StartActivity.java
+++ app/src/main/java/com/example/rspp/StartActivity.java
@@ -1,27 +1,40 @@
 package com.example.rspp;
 
+import android.graphics.drawable.AnimatedImageDrawable;
+import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class StartActivity extends AppCompatActivity {
 
-    Button bt_r;
-    Button bt_s;
-    Button bt_p;
+    AnimationDrawable hand_animation;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
 
-        bt_r = findViewById(R.id.bt_r);
-        bt_s = findViewById(R.id.bt_s);
-        bt_p = findViewById(R.id.bt_p);
+        ImageView hand_ani = (ImageView) findViewById(R.id.rsp_ani_image_view);
+        hand_ani.setBackgroundResource(R.drawable.hand_ani);
+        hand_animation = (AnimationDrawable) hand_ani.getBackground();
+
+        hand_ani.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                hand_animation.start();
+            }
+        });
+
+
+
+
 
 
 
