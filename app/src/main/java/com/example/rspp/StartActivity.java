75fbbcb2e9deb0cc3d3cdccdc48920f349e62386
uhunn <wxvv3386@gmail.com>
2020-05-22 19:58:56 +0900

    r 버튼 누르면 이미지 변환

--- app/src/main/java/com/example/rspp/StartActivity.java
+++ app/src/main/java/com/example/rspp/StartActivity.java
@@ -7,38 +7,59 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Switch;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class StartActivity extends AppCompatActivity {
 
+
+    ImageView hand_ani;
+    ImageView hand_com;
     AnimationDrawable hand_animation;
+
+    int i = 3;
+
+
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
+        hand_ani = findViewById(R.id.rsp_ani_image_view);
+        hand_com = findViewById(R.id.hand_computer);
 
-        ImageView hand_ani = (ImageView) findViewById(R.id.rsp_ani_image_view);
-        hand_ani.setBackgroundResource(R.drawable.hand_ani);
-        hand_animation = (AnimationDrawable) hand_ani.getBackground();
 
-        hand_ani.setOnClickListener(new View.OnClickListener() {
+        hand_animation = (AnimationDrawable) hand_ani.getDrawable();
+        hand_animation.start();
+
+        Button bt = (Button)findViewById(R.id.bt_r);
+        bt.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                hand_animation.start();
+
+                hand_ani.setVisibility(View.GONE);
+                hand_com.setVisibility(View.VISIBLE);
+
+
+
+
+
+
+
+
             }
-        });
-
-
-
-
-
-
-
-
+        }) ;
 
     }
+
+
+
+
+
+
+
 }
+
